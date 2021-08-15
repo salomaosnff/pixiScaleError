817fe680595d21fcdb6f2433b683efac29c347ff
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-08-15 16:37:51 +0300

    lalallaaa

---
+++ ts/config.ts
@@ -0,0 +1,8 @@
+export const W = 500;
+export const H = 275;
+
+export const stoneInfo = {
+  width: 62,
+  height: 85,
+}
+
---
+++ ts/interface.ts
@@ -0,0 +1,49 @@
+import * as PIXI from 'pixi.js';
+
+
+export interface StoneInfo {
+  c: Color,
+  n: number,
+  id: number,
+  fo?: boolean,
+  o?: boolean
+}
+interface Coordinate {
+  x: number,
+  y: number
+}
+export interface UserInfoForStones {
+  [index: string]: {
+    ply: Coordinate,
+    next: {
+      i: number,
+      yer: Coordinate
+
+    }
+    prev: {
+      i: number,
+      yer: Coordinate
+
+    }
+  }
+}
+export interface StoneExtra {
+  move: boolean,
+  dontMove: boolean,
+  prevX: number,
+  prevY: number,
+  indexXY?: String,
+  stoneInfo: StoneInfo
+
+}
+export interface Stone extends PIXI.Sprite {
+  extra: StoneExtra
+}
+
+export enum Color {
+  Red = "Red",
+  Yellow = "Yellow",
+  Blue = "Blue",
+  Black = "Black",
+  White = "White"
+}
